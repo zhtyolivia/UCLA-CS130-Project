b897446e51b9d7aac834a66bc7777c8a2654c8b3
Ethan_Jiang <1766569133@qq.com>
2024-03-10 18:20:23 -0700

    driver can see passenger post

--- frontend/src/components/RideshareCard/RideshareCard.js
+++ frontend/src/components/RideshareCard/RideshareCard.js
@@ -7,11 +7,10 @@ function RideshareCard({ id, startingLocation, endingLocation, availableSeats, s
   return (
     <Link to={`/driverposts/${id}`} className="post-link" style={{ textDecoration: 'none', color: 'inherit' }}>
       <div className="post">
-        {/* <h2 className="post-title">{title}</h2> */}
         <div className="post-details">
           <p><strong>Starting Location:</strong> {startingLocation}</p>
           <p><strong>Ending Location:</strong> {endingLocation}</p>
-          <p><strong>Start time:</strong> {convertDate2Readable(startTime)}</p>
+          <p><strong>Start time:</strong> {startTime}</p> {/* startTime is now already formatted */}
           <p><strong>Available Seats:</strong> {availableSeats}</p>
         </div>
         <p className="post-content">{content}</p>
